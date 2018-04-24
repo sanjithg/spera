94f908d7542acf20eb819dbc9c6bf96ab547937f
Nikhil <nikhil.giddaluru@valuelabs.com>
2018-04-24 20:28:33 +0530

    new commit

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,9 +3,7 @@ import speraLogo from '../assets/images/spera-logo.png';
 import { Image } from 'react-bootstrap';
 
 const logo = (props) => (
-    <div className='Container'>
       <Image className={props.cssClass} src = {speraLogo}/>
-    </div>
 );
 
 export default logo;
--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -1,23 +1,49 @@
 import React, {Component} from 'react';
-import Header from '../components/Header';
-import Spinner from '../components/Spinner'
-import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+import Header from '../components/Headers/Header';
+import Sliders from '../components/Sliders/Sliders';
+import Spinner from '../components/Spinner';
+import Features from '../components/Features/Features';
+import AboutUs from '../components/AboutUs';
+import { Button } from 'react-bootstrap';
 
 
 class Layout extends Component {
   state = {
+    sliderImages: ["../../assets/images/sliderImages/slider-1.jpg", "../../assets/images/sliderImages/slider-2.jpg"],
     loading: false
   };
 
   render(){
+    const features = [{
+      key: 1,
+      icon: "fa fa-home",
+      heading: "Professional Build",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod"
+    }, {
+      key: 2,
+      icon: "fa fa-building",
+      heading: "We Deliver Quality",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod"
+    },  {
+      key: 3,
+      icon: "fa fa-clock-o",
+      heading: "Always On Time",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod"
+    }, {
+      key: 4,
+      icon: "fa fa-thumbs-up",
+      heading: "We Are Passionate",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod"
+    }];
     const spinner =  this.state.loading? <Spinner /> : null
 
     return(
       <div >
         {spinner}
         <Header />
-        <Button bsStyle="primary">Button</Button>
-
+        <Sliders sliders={this.state.sliderImages}/>
+        <Features features = {features}/>
+        <AboutUs />
       </div>
     )
   }
